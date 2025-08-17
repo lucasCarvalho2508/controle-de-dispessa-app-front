8e2ab3d8a03a75471ff5d0e7efc22913a9c5ad03
Lucas Assis <lucascarvalhodeassis25.08@gmail.com>
2025-08-17 15:36:39 -0300

    component form dispesas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { DispesaFormComponent } from './dispesa-form/dispesa-form.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DispesaFormComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
